1b889142c9fb5da8976b1de4eb76f5c1551359c5
tesfish2012 <tesfay3814@email.com>
2024-07-15 16:41:32 +0300

    update changes

--- src/views/protected/DocumentsView.vue
+++ src/views/protected/DocumentsView.vue
@@ -1,138 +1,5 @@
-<script setup>
-import ApiService from '@/services/apiService'
-
-import { BASE_UPLOAD } from '@/config'
-
-import BaseInput from '@/components/base/BaseInput.vue'
-import BaseButton from '@/components/base/BaseButton.vue'
-import BaseTextarea from '@/components/base/BaseTextarea.vue'
-import BaseFileInput from '@/components/base/BaseFileInput.vue'
-
-import { ref, onMounted } from 'vue'
-
-// let language = ref(localStorage.getItem('lang') || '')
-const documents = ref([])
-const fetchDocuments = async () => {
-  try {
-    const response = await ApiService.get('/admin/documents')
-    if (response) {
-      documents.value = response.data.map((item) => ({
-        ...item,
-        title: JSON.parse(item.title),
-        description : JSON.parse(item.description )
-      }))
-    }
-  } catch (error) {
-    if (error.response && error.response.data && error.response.status === 404) {
-      return
-    } else {
-      setTimeout(() => {}, 2000)
-    }
-  }
-}
-
-let showAddModal = ref(false)
-const currentLanguage = ref('en')
-const toggleLanguage = (lang) => {
-  currentLanguage.value = lang
-}
-const form = ref({
-  id: null,
-  title: { en: '', ti: '', am: '' },
-  description : { en: '', ti: '', am: '' },
-  document: ''
-})
-
-let logo = ref('')
-const handleFileChange = (file) => {
-  form.value.document = file
-  logo.value = file.name
-}
-
-const errorMessage = ref('')
-const successMessage = ref('')
-
-let edit = ref(false)
-const editDocument = (document) => {
-  form.value = {
-    id: document.id,
-    title: { ...document.title },
-    description : { ...document.description  },
-    document: document.document
-  }
-  edit.value = true
-  showAddModal.value = true
-}
-
-const saveDocument = async () => {
-  console.log(form.value)
-  const formData = new FormData()
-  formData.append('title', JSON.stringify(form.value.title))
-  formData.append('description', JSON.stringify(form.value.description ))
-  if (form.value.document) {
-    formData.append('document', form.value.document)
-  }
-  try {
-    if (edit.value) {
-      const response = await ApiService.patch('/admin/documents/' + form.value.id, formData)
-      if (response.success) {
-        successMessage.value = response.message
-        fetchDocuments()
-        closeModal()
-      } else {
-        errorMessage.value = 'Failed to save Document'
-      }
-    } else {
-      const res = await ApiService.post('/admin/documents', formData)
-      if (res.success) {
-        successMessage.value = res.message
-        fetchDocuments()
-        closeModal()
-      } else {
-        errorMessage.value = 'Failed to save Document'
-      }
-    }
-  } catch (error) {
-    errorMessage.value = 'Failed to save Document'
-  }
-}
-
-const deleteDocument = async (id) => {
-   const sure = window.confirm('Are you sure to delete this team?')
-  if (sure) {
-  try {
-    const response = await ApiService.delete('/admin/services/' + id)
-
-    if (response.success) {
-      fetchDocuments()
-    } else {
-      errorMessage.value = 'Failed to save Partner'
-    }
-  } catch (error) {
-    errorMessage.value = 'Failed to save Partner'
-  }
-}
-}
-
-const closeModal = () => {
-  showAddModal.value = false
-  form.value = {
-    id: null,
-    title: { en: '', ti: '', am: '' },
-    description : { en: '', ti: '', am: '' },
-    document: ''
-  }
-  edit.value = false
-  logo.value = ''
-}
-
-onMounted(() => {
-  fetchDocuments()
-})
-</script>
-
 <template>
-  <section class="w-[82%] px-[6%] py-12 flex flex-col items-center gap-4 bg-white rounded-2xl">
+  <section class="w-full px-6 py-12 flex flex-col items-center gap-4 bg-white rounded-2xl">
     <!-- Documents -->
     <button
       @click="showAddModal = true"
@@ -149,13 +16,13 @@ onMounted(() => {
       <div
         v-for="(document, i) in documents"
         :key="i"
-        class="p-4 flex flex-col gap-2 zbg-white hover:bg-[#53900F] hover:text-white justify-between shadow-xl bg-[#53900F]/10"
+        class="p-4 flex flex-col gap-2 bg-white hover:bg-[#53900F] hover:text-white justify-between shadow-xl"
       >
-      <h1 class="text-2xl font-bold">{{ document.title[currentLanguage] }}</h1>
-        <p class="line-clamp-5">
-          {{ document.description [currentLanguage] }}
-        </p>
-      <a  :href="BASE_UPLOAD + document.document">{{ document.document }}</a>
+        <h1 class="text-2xl font-bold">{{ document.title[currentLanguage] }}</h1>
+        <p class="line-clamp-5">{{ document.description[currentLanguage] }}</p>
+        <a :href="BASE_UPLOAD + document.document" target="_blank" class="text-blue-500 underline">
+          {{ document.document }}
+        </a>
         <div class="flex gap-2 justify-end">
           <button @click="editDocument(document)">
             <font-awesome-icon icon="edit" class="text-blue-500"></font-awesome-icon>
@@ -169,13 +36,13 @@ onMounted(() => {
 
     <div
       v-if="showAddModal"
-      class="fixed inset-0 overflow-auto flex items-center z-50 justify-center modal bg-black/50 bg-opacity-50 modal"
+      class="fixed inset-0 overflow-auto flex items-center z-50 justify-center modal bg-black/50 bg-opacity-50"
     >
       <div class="bg-white shadow-lg w-full max-w-md p-6">
         <div class="text-center">
           <div class="flex justify-between">
             <h3 class="text-lg font-medium text-gray-900">
-              {{ edit ? 'Edit document' : 'Add document' }}
+              {{ edit ? 'Edit Document' : 'Add Document' }}
             </h3>
             <button @click="closeModal" type="button" class="p-1 text-white bg-gray-500 rounded">
               Cancel
@@ -194,15 +61,15 @@ onMounted(() => {
                 English
               </button>
               <button
-              @click="toggleLanguage('ti')"
-              :class="{
-                'border-2 border-b-[#53900F]': currentLanguage === 'ti',
-                'bg-gray-200': currentLanguage !== 'ti'
-              }"
-              class="px-4 py-1"
-            >
-              ትግርኛ
-            </button>
+                @click="toggleLanguage('ti')"
+                :class="{
+                  'border-2 border-b-[#53900F]': currentLanguage === 'ti',
+                  'bg-gray-200': currentLanguage !== 'ti'
+                }"
+                class="px-4 py-1"
+              >
+                ትግርኛ
+              </button>
               <button
                 @click="toggleLanguage('am')"
                 :class="{
@@ -225,19 +92,19 @@ onMounted(() => {
                 ></BaseInput>
 
                 <BaseTextarea
-                  v-model="form.description [currentLanguage]"
+                  v-model="form.description[currentLanguage]"
                   inputClass="p-2 border border-gray-300 rounded"
                   placeholder="Document description"
                 ></BaseTextarea>
               </div>
               <div class="flex justify-end gap-2 flex-col">
                 <BaseFileInput
-                  @image-update="handleFileChange($event)"
+                  @change="handleFileChange"
                   label="Attach Document"
-                    type="file"
-                    inputClass="p-2 border border-gray-300 rounded"
-                    placeholder="Image"
-                    accept="application/pdf"
+                  type="file"
+                  inputClass="p-2 border border-gray-300 rounded"
+                  placeholder="Image"
+                  accept="application/pdf"
                 ></BaseFileInput>
                 <span>{{ logo }}</span>
                 <BaseButton type="submit" class="w-full px-2 py-2 rounded">
@@ -251,6 +118,134 @@ onMounted(() => {
     </div>
   </section>
 </template>
+
+<script setup>
+import ApiService from '@/services/apiService'
+import { BASE_UPLOAD } from '@/config'
+import BaseInput from '@/components/base/BaseInput.vue'
+import BaseButton from '@/components/base/BaseButton.vue'
+import BaseTextarea from '@/components/base/BaseTextarea.vue'
+import BaseFileInput from '@/components/base/BaseFileInput.vue'
+import { ref, onMounted } from 'vue'
+
+const documents = ref([])
+const fetchDocuments = async () => {
+  try {
+    const response = await ApiService.get('/admin/documents')
+    if (response) {
+      documents.value = response.data.map((item) => ({
+        ...item,
+        title: JSON.parse(item.title),
+        description: JSON.parse(item.description)
+      }))
+    }
+  } catch (error) {
+    if (error.response && error.response.data && error.response.status === 404) {
+      return
+    } else {
+      setTimeout(() => {}, 2000)
+    }
+  }
+}
+
+const showAddModal = ref(false)
+const currentLanguage = ref('en')
+const toggleLanguage = (lang) => {
+  currentLanguage.value = lang
+}
+const form = ref({
+  id: null,
+  title: { en: '', ti: '', am: '' },
+  description: { en: '', ti: '', am: '' },
+  document: ''
+})
+
+const logo = ref('')
+const handleFileChange = (event) => {
+  const file = event.target.files[0]
+  form.value.document = file
+  logo.value = file.name
+}
+
+const errorMessage = ref('')
+const successMessage = ref('')
+
+const edit = ref(false)
+const editDocument = (document) => {
+  form.value = {
+    id: document.id,
+    title: { ...document.title },
+    description: { ...document.description },
+    document: document.document
+  }
+  edit.value = true
+  showAddModal.value = true
+}
+
+const saveDocument = async () => {
+  const formData = new FormData()
+  formData.append('title', JSON.stringify(form.value.title))
+  formData.append('description', JSON.stringify(form.value.description))
+  if (form.value.document instanceof File) {
+    formData.append('document', form.value.document)
+  }
+  try {
+    if (edit.value) {
+      const response = await ApiService.patch('/admin/documents/' + form.value.id, formData)
+      if (response.data.success) {
+        successMessage.value = response.data.message
+        fetchDocuments()
+        closeModal()
+      } else {
+        errorMessage.value = 'Failed to save Document'
+      }
+    } else {
+      const res = await ApiService.post('/admin/documents', formData)
+      if (res.data.success) {
+        successMessage.value = res.data.message
+        fetchDocuments()
+        closeModal()
+      } else {
+        errorMessage.value = 'Failed to save Document'
+      }
+    }
+  } catch (error) {
+    errorMessage.value = 'Failed to save Document'
+  }
+}
+
+const deleteDocument = async (id) => {
+  const sure = window.confirm('Are you sure to delete this document?')
+  if (sure) {
+    try {
+      const response = await ApiService.delete('/admin/documents/' + id)
+      if (response.data.success) {
+        fetchDocuments()
+      } else {
+        errorMessage.value = 'Failed to delete Document'
+      }
+    } catch (error) {
+      errorMessage.value = 'Failed to delete Document'
+    }
+  }
+}
+
+const closeModal = () => {
+  showAddModal.value = false
+  form.value = {
+    id: null,
+    title: { en: '', ti: '', am: '' },
+    description: { en: '', ti: '', am: '' },
+    document: ''
+  }
+  edit.value = false
+  logo.value = ''
+}
+
+onMounted(() => {
+  fetchDocuments()
+})
+</script>
 
 <style>
 .modal {
--- src/views/protected/StrategyView.vue
+++ src/views/protected/StrategyView.vue
@@ -1,138 +1,6 @@
-<script setup>
-import ApiService from '@/services/apiService'
-
-import { BASE_AVATAR } from '@/config'
-
-import BaseInput from '@/components/base/BaseInput.vue'
-import BaseButton from '@/components/base/BaseButton.vue'
-import BaseTextarea from '@/components/base/BaseTextarea.vue'
-import BaseFileInput from '@/components/base/BaseFileInput.vue'
-
-import { ref, onMounted } from 'vue'
-
-// let language = ref(localStorage.getItem('lang') || '')
-const services = ref([])
-const fetchServices = async () => {
-  try {
-    const response = await ApiService.get('/admin/services')
-    if (response) {
-      services.value = response.data.map((item) => ({
-        ...item,
-        serviceTitle: JSON.parse(item.serviceTitle),
-        serviceDescription: JSON.parse(item.serviceDescription)
-      }))
-    }
-  } catch (error) {
-    if (error.response && error.response.data && error.response.status === 404) {
-      return
-    } else {
-      setTimeout(() => {}, 2000)
-    }
-  }
-}
-
-let showAddModal = ref(false)
-const currentLanguage = ref('en')
-const toggleLanguage = (lang) => {
-  currentLanguage.value = lang
-}
-const form = ref({
-  id: null,
-  serviceTitle: { en: '', ti: '', am: '' },
-  serviceDescription: { en: '', ti: '', am: '' },
-  serviceImage: ''
-})
-
-let logo = ref('')
-const handleFileChange = (file) => {
-  form.value.serviceImage = file
-  logo.value = file.name
-}
-
-const errorMessage = ref('')
-const successMessage = ref('')
-
-let edit = ref(false)
-const editService = (service) => {
-  form.value = {
-    id: service.id,
-    serviceTitle: { ...service.serviceTitle },
-    serviceDescription: { ...service.serviceDescription },
-    serviceImage: service.serviceImage
-  }
-  edit.value = true
-  showAddModal.value = true
-}
-
-const saveService = async () => {
-  const formData = new FormData()
-  formData.append('serviceTitle', JSON.stringify(form.value.serviceTitle))
-  formData.append('serviceDescription', JSON.stringify(form.value.serviceDescription))
-  if (form.value.serviceImage) {
-    formData.append('serviceImage', form.value.serviceImage)
-  }
-  try {
-    if (edit.value) {
-      const response = await ApiService.patch('/admin/services/' + form.value.id, formData)
-      if (response.success) {
-        successMessage.value = response.message
-        fetchServices()
-        closeModal()
-      } else {
-        errorMessage.value = 'Failed to save Service'
-      }
-    } else {
-      const res = await ApiService.post('/admin/services', formData)
-      if (res.success) {
-        successMessage.value = res.message
-        fetchServices()
-        closeModal()
-      } else {
-        errorMessage.value = 'Failed to save Service'
-      }
-    }
-  } catch (error) {
-    errorMessage.value = 'Failed to save Service'
-  }
-}
-
-const deleteService = async (id) => {
-   const sure = window.confirm('Are you sure to delete this team?')
-  if (sure) {
-  try {
-    const response = await ApiService.delete('/admin/services/' + id)
-
-    if (response.success) {
-      fetchServices()
-    } else {
-      errorMessage.value = 'Failed to save Partner'
-    }
-  } catch (error) {
-    errorMessage.value = 'Failed to save Partner'
-  }
-}
-}
-
-const closeModal = () => {
-  showAddModal.value = false
-  form.value = {
-    id: null,
-    serviceTitle: { en: '', ti: '', am: '' },
-    serviceDescription: { en: '', ti: '', am: '' },
-    serviceImage: ''
-  }
-  edit.value = false
-  logo.value = ''
-}
-
-onMounted(() => {
-  fetchServices()
-})
-</script>
-
 <template>
-  <section class="w-[82%] px-[6%] py-12 flex flex-col items-center gap-4 bg-white rounded-2xl">
-    <!-- Services -->
+  <section class="w-full px-6 py-12 flex flex-col items-center gap-4 bg-white rounded-2xl">
+    <!-- Documents -->
     <button
       @click="showAddModal = true"
       class="text-[#539000] self-end border flex items-center px-2 py-1 border-[#539000]"
@@ -141,33 +9,25 @@ onMounted(() => {
         icon="add"
         class="bg-white text-[#539000] p-2 rounded-full"
       ></font-awesome-icon>
-      Add Service
+      Add Document
     </button>
 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 place-content-center">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
       <div
-        v-for="(service, i) in services"
+        v-for="(document, i) in documents"
         :key="i"
-        class="p-4 flex flex-col gap-2 zbg-white hover:bg-[#53900F] hover:text-white justify-between shadow-xl bg-[#53900F]/10"
+        class="p-4 flex flex-col gap-2 bg-white hover:bg-[#53900F] hover:text-white justify-between shadow-xl"
       >
-        <img
-          v-if="service.serviceImage"
-          :src="BASE_AVATAR + service.serviceImage"
-          alt=""
-          class="w-24 h-24 ring-2 ring-yellow-300 rounded-full mx-auto"
-        />
-        <p v-else class="w-20 h-20 rounded-full text-6xl">
-          {{ service.serviceTitle[currentLanguage] }}
-        </p>
-        <h1 class="text-2xl font-bold">{{ service.serviceTitle[currentLanguage] }}</h1>
-        <p class="line-clamp-5">
-          {{ service.serviceDescription[currentLanguage] }}
-        </p>
+        <h1 class="text-2xl font-bold">{{ document.title[currentLanguage] }}</h1>
+        <p class="line-clamp-5">{{ document.description[currentLanguage] }}</p>
+        <a :href="BASE_UPLOAD + document.document" target="_blank" class="text-blue-500 underline">
+          {{ document.document }}
+        </a>
         <div class="flex gap-2 justify-end">
-          <button @click="editService(service)">
+          <button @click="editDocument(document)">
             <font-awesome-icon icon="edit" class="text-blue-500"></font-awesome-icon>
           </button>
-          <button @click="deleteService(service.id)">
+          <button @click="deleteDocument(document.id)">
             <font-awesome-icon icon="trash" class="text-red-500"></font-awesome-icon>
           </button>
         </div>
@@ -176,13 +36,13 @@ onMounted(() => {
 
     <div
       v-if="showAddModal"
-      class="fixed inset-0 overflow-auto flex items-center z-50 justify-center modal bg-black/50 bg-opacity-50 modal"
+      class="fixed inset-0 overflow-auto flex items-center z-50 justify-center modal bg-black/50 bg-opacity-50"
     >
       <div class="bg-white shadow-lg w-full max-w-md p-6">
         <div class="text-center">
           <div class="flex justify-between">
             <h3 class="text-lg font-medium text-gray-900">
-              {{ edit ? 'Edit Service' : 'Add Service' }}
+              {{ edit ? 'Edit Document' : 'Add Document' }}
             </h3>
             <button @click="closeModal" type="button" class="p-1 text-white bg-gray-500 rounded">
               Cancel
@@ -201,15 +61,15 @@ onMounted(() => {
                 English
               </button>
               <button
-              @click="toggleLanguage('ti')"
-              :class="{
-                'border-2 border-b-[#53900F]': currentLanguage === 'ti',
-                'bg-gray-200': currentLanguage !== 'ti'
-              }"
-              class="px-4 py-1"
-            >
-              ትግርኛ
-            </button>
+                @click="toggleLanguage('ti')"
+                :class="{
+                  'border-2 border-b-[#53900F]': currentLanguage === 'ti',
+                  'bg-gray-200': currentLanguage !== 'ti'
+                }"
+                class="px-4 py-1"
+              >
+                ትግርኛ
+              </button>
               <button
                 @click="toggleLanguage('am')"
                 :class="{
@@ -221,34 +81,34 @@ onMounted(() => {
                 አማርኛ
               </button>
             </div>
-            <form @submit.prevent="saveService" class="flex flex-col gap-4">
+            <form @submit.prevent="saveDocument" class="flex flex-col gap-4">
               <div class="flex flex-col gap-6">
                 <BaseInput
-                  v-model="form.serviceTitle[currentLanguage]"
+                  v-model="form.title[currentLanguage]"
                   type="text"
                   required
                   inputClass="p-2 border border-gray-300 rounded"
-                  :placeholder="$t('Service Title')"
+                  :placeholder="$t('Document title')"
                 ></BaseInput>
 
                 <BaseTextarea
-                  v-model="form.serviceDescription[currentLanguage]"
+                  v-model="form.description[currentLanguage]"
                   inputClass="p-2 border border-gray-300 rounded"
-                  placeholder="Service Description"
+                  placeholder="Document description"
                 ></BaseTextarea>
               </div>
               <div class="flex justify-end gap-2 flex-col">
                 <BaseFileInput
-                  @image-update="handleFileChange($event)"
-                  label="Add Picture"
-                    type="file"
-                    inputClass="p-2 border border-gray-300 rounded"
-                    placeholder="Image"
-                    accept="image/*"
+                  @change="handleFileChange"
+                  label="Attach Document"
+                  type="file"
+                  inputClass="p-2 border border-gray-300 rounded"
+                  placeholder="Image"
+                  accept="application/pdf"
                 ></BaseFileInput>
                 <span>{{ logo }}</span>
                 <BaseButton type="submit" class="w-full px-2 py-2 rounded">
-                  Save Service
+                  Save Document
                 </BaseButton>
               </div>
             </form>
@@ -258,6 +118,134 @@ onMounted(() => {
     </div>
   </section>
 </template>
+
+<script setup>
+import ApiService from '@/services/apiService'
+import { BASE_UPLOAD } from '@/config'
+import BaseInput from '@/components/base/BaseInput.vue'
+import BaseButton from '@/components/base/BaseButton.vue'
+import BaseTextarea from '@/components/base/BaseTextarea.vue'
+import BaseFileInput from '@/components/base/BaseFileInput.vue'
+import { ref, onMounted } from 'vue'
+
+const documents = ref([])
+const fetchDocuments = async () => {
+  try {
+    const response = await ApiService.get('/admin/documents')
+    if (response) {
+      documents.value = response.data.map((item) => ({
+        ...item,
+        title: JSON.parse(item.title),
+        description: JSON.parse(item.description)
+      }))
+    }
+  } catch (error) {
+    if (error.response && error.response.data && error.response.status === 404) {
+      return
+    } else {
+      setTimeout(() => {}, 2000)
+    }
+  }
+}
+
+const showAddModal = ref(false)
+const currentLanguage = ref('en')
+const toggleLanguage = (lang) => {
+  currentLanguage.value = lang
+}
+const form = ref({
+  id: null,
+  title: { en: '', ti: '', am: '' },
+  description: { en: '', ti: '', am: '' },
+  document: ''
+})
+
+const logo = ref('')
+const handleFileChange = (event) => {
+  const file = event.target.files[0]
+  form.value.document = file
+  logo.value = file.name
+}
+
+const errorMessage = ref('')
+const successMessage = ref('')
+
+const edit = ref(false)
+const editDocument = (document) => {
+  form.value = {
+    id: document.id,
+    title: { ...document.title },
+    description: { ...document.description },
+    document: document.document
+  }
+  edit.value = true
+  showAddModal.value = true
+}
+
+const saveDocument = async () => {
+  const formData = new FormData()
+  formData.append('title', JSON.stringify(form.value.title))
+  formData.append('description', JSON.stringify(form.value.description))
+  if (form.value.document instanceof File) {
+    formData.append('document', form.value.document)
+  }
+  try {
+    if (edit.value) {
+      const response = await ApiService.patch('/admin/documents/' + form.value.id, formData)
+      if (response.data.success) {
+        successMessage.value = response.data.message
+        fetchDocuments()
+        closeModal()
+      } else {
+        errorMessage.value = 'Failed to save Document'
+      }
+    } else {
+      const res = await ApiService.post('/admin/documents', formData)
+      if (res.data.success) {
+        successMessage.value = res.data.message
+        fetchDocuments()
+        closeModal()
+      } else {
+        errorMessage.value = 'Failed to save Document'
+      }
+    }
+  } catch (error) {
+    errorMessage.value = 'Failed to save Document'
+  }
+}
+
+const deleteDocument = async (id) => {
+  const sure = window.confirm('Are you sure to delete this document?')
+  if (sure) {
+    try {
+      const response = await ApiService.delete('/admin/documents/' + id)
+      if (response.data.success) {
+        fetchDocuments()
+      } else {
+        errorMessage.value = 'Failed to delete Document'
+      }
+    } catch (error) {
+      errorMessage.value = 'Failed to delete Document'
+    }
+  }
+}
+
+const closeModal = () => {
+  showAddModal.value = false
+  form.value = {
+    id: null,
+    title: { en: '', ti: '', am: '' },
+    description: { en: '', ti: '', am: '' },
+    document: ''
+  }
+  edit.value = false
+  logo.value = ''
+}
+
+onMounted(() => {
+  fetchDocuments()
+})
+</script>
 
 <style>
 .modal {
