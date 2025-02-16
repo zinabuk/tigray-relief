996ad31d73aaf809a2ff4b3645cdc8dd01b517d0
zinabuk <kalayuzinabu@gmail.com>
2025-02-16 15:27:47 +0300

    updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -340,7 +340,7 @@ onMounted(() => {
   <section
     class="w-full px-[2%] py-12 flex flex-col items-center gap-4 bg-[#539000]/5 overflow-hidden"
   >
-    <h1 class="text-3xl font-bold">{{ $t('News and stories from us') }}</h1>
+    <h1 class="text-3xl font-bold">{{ $t('News and Stories') }}</h1>
     <div class="w-full grid grid-cols-1 md:grid-cols-2 gap-4">
       <div
         v-for="(event, i) in blogs.slice(0, 4)"
